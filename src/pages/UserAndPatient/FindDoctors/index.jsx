f938c635ea502cb08663b3ba2c6f1732c6b16f8b
Adnan Hashim <adnanhashim235@gmail.com>
2026-03-16 16:16:35 +0500

    integrate api

--- src/pages/UserAndPatient/FindDoctors/index.jsx
+++ src/pages/UserAndPatient/FindDoctors/index.jsx
@@ -1,41 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Search, MapPin, Clock, Star, Heart } from "lucide-react";
 import Button from "@/components/ui/Button";
 import Card from "@/components/ui/Card";
 import PageHeader from "@/components/ui/PageHeader";
 import { LoaderCenter } from "@/components/ui/Loader";
 import { useNavigate } from "react-router-dom";
-import { useSearchDoctorsQuery } from "@/services";
+import {
+  useAddFavoriteMutation,
+  useAllDoctorsQuery,
+  useRemoveFavoriteMutation,
+} from "@/services";
+import { toastSuccess } from "@/components/ui/Toast";
 const SPECIALTIES = [
   "All",
   "Cardiologist",
-  "Pediatrician",
   "Dermatologist",
-  "Neurologist",
-  "Orthopedic",
-  "General Practitioner",
-  "Gynecologist",
+  "ENT",
+  "Pediatrician",
+  "Pulmonologist",
+  "Gastroenterologist",
+  "Oncologist",
+  "Allergist",
+  "Physiotherapist",
+  "Dentist",
 ];
 
 export default function FindDoctors() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState("");
   const [limit, setLimit] = useState(12);
-  const [page, setPage] = useState(1);
   const [selectedSpecialty, setSelectedSpecialty] = useState("All");
-  const [favorites, setFavorites] = useState(new Set());
+
+  const [loadingFavoriteId, setLoadingFavoriteId] = useState(null);
   const navigate = useNavigate();
+  const [addFavorite] = useAddFavoriteMutation();
+  const [removeFavorite] = useRemoveFavoriteMutation();
   const {
     data: doctors,
     isLoading,
     isFetching,
     error,
     refetch,
-  } = useSearchDoctorsQuery({
-    search: searchQuery,
-    limit,
-    page,
-    specialty: selectedSpecialty === "All" ? undefined : selectedSpecialty,
+  } = useAllDoctorsQuery({
+    page: 1,
+    limit: limit,
+    search:
+      selectedSpecialty !== "All" ? selectedSpecialty : debouncedSearchQuery,
   });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearchQuery(searchQuery);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
 
   const mapApiDoctorToCard = (d) => {
     const id = d._id;
@@ -47,6 +66,7 @@ export default function FindDoctors() {
     const fee = d.consultationFee || 0;
     const rating = d.rating || "N/A";
     const reviews = d.reviews || "N/A";
+    const isFavorite = d.isFavorite;
     const availableArr = Array.isArray(d.availableDayAndTime)
       ? d.availableDayAndTime
       : [];
@@ -64,6 +84,7 @@ export default function FindDoctors() {
       availableArr,
       fee,
       type,
+      isFavorite,
     };
   };
 
@@ -71,19 +92,48 @@ export default function FindDoctors() {
 
   const sourceDoctors =
     doctors?.data?.doctors?.length > 0 ? apiList.map(mapApiDoctorToCard) : [];
-  const toggleFavorite = (id) => {
-    const newFavorites = new Set(favorites);
-    if (newFavorites.has(id)) {
-      newFavorites.delete(id);
-    } else {
-      newFavorites.add(id);
-    }
-    setFavorites(newFavorites);
-  };
 
   const handleLoadMore = () => {
-    if (limit >= (doctors?.data?.count || 0)) return;
+    if (limit >= (doctors?.data?.totalDoctors || 0)) return;
     setLimit((prev) => prev + 12);
+  };
+  const addToFavorites = async (doctorId) => {
+    const payload = {
+      doctorId: doctorId,
+      type: "doctor",
+    };
+    setLoadingFavoriteId(doctorId);
+    try {
+      const response = await addFavorite({ body: payload });
+
+      if (response) {
+        toastSuccess(response.message || "Added to favorites");
+        refetch();
+      }
+    } catch (error) {
+      console.error("Error toggling favorite:", error);
+    } finally {
+      setLoadingFavoriteId(null);
+    }
+  };
+  const removeFromFavorites = async (doctorId) => {
+    const payload = {
+      doctorId: doctorId,
+      type: "doctor",
+    };
+    setLoadingFavoriteId(doctorId);
+    try {
+      const response = await removeFavorite({ body: payload });
+
+      if (response) {
+        toastSuccess(response.message || "Removed from favorites");
+        refetch();
+      }
+    } catch (error) {
+      console.error("Error toggling favorite:", error);
+    } finally {
+      setLoadingFavoriteId(null);
+    }
   };
 
   return (
@@ -115,7 +165,7 @@ export default function FindDoctors() {
             </Button>
           </div>
           <div className="flex flex-wrap gap-2 overflow-x-auto pb-2">
-            {SPECIALTIES.map((specialty) => (
+            {SPECIALTIES?.map((specialty) => (
               <button
                 key={specialty}
                 onClick={() => setSelectedSpecialty(specialty)}
@@ -134,14 +184,8 @@ export default function FindDoctors() {
       <div className=" px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex justify-between items-center mb-6">
           <p className="text-muted-foreground">
-            {sourceDoctors.length} doctors found
+            {doctors?.data?.totalDoctors} doctors found
           </p>
-          <select className="px-3 py-2 rounded-lg border border-input bg-background text-foreground text-sm">
-            <option>Sort by: Recommended</option>
-            <option>Rating: High to Low</option>
-            <option>Price: Low to High</option>
-            <option>Experience: Most</option>
-          </select>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
           {isLoading ? (
@@ -152,7 +196,6 @@ export default function FindDoctors() {
             <div className="col-span-full">
               <Card padding="none" className="p-6 text-center">
                 <p className="text-red-600 mb-4">
-                  Failed to load doctors.{" "}
                   {error?.data?.message || error?.message}
                 </p>
                 <div className="flex justify-center">
@@ -165,97 +208,108 @@ export default function FindDoctors() {
               No doctors found. Try adjusting your search or filters.
             </div>
           ) : (
-            sourceDoctors.map((doctor) => (
-              <Card
-                key={doctor.id}
-                hover={true}
-                padding="none"
-                className="cursor-pointer h-full flex flex-col overflow-hidden"
-              >
-                <div className="bg-linear-to-r from-emerald-50 to-blue-50 p-4 flex items-start gap-4">
-                  <img
-                    src={doctor.image || "/placeholder.svg"}
-                    alt={doctor.name}
-                    className="w-16 h-16 rounded-lg object-cover"
-                  />
-                  <div className="flex-1">
-                    <h3 className="font-bold text-foreground text-lg">
-                      {doctor.name}
-                    </h3>
-                    <p className="text-sm text-muted-foreground">
-                      {doctor.specialty}
-                    </p>
-                  </div>
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      toggleFavorite(doctor.id);
-                    }}
-                    className="text-muted-foreground hover:text-red-500 transition-colors"
-                  >
-                    <Heart
-                      className="w-5 h-5"
-                      fill={favorites.has(doctor.id) ? "currentColor" : "none"}
+            sourceDoctors?.map((doctor) => {
+              return (
+                <Card
+                  key={doctor.id}
+                  hover={true}
+                  padding="none"
+                  className="cursor-pointer h-full flex flex-col overflow-hidden"
+                >
+                  <div className="bg-linear-to-r from-emerald-50 to-blue-50 p-4 flex items-start gap-4">
+                    <img
+                      src={doctor.image || "/placeholder.svg"}
+                      alt={doctor.name}
+                      className="w-16 h-16 rounded-lg object-cover"
                     />
-                  </button>
-                </div>
-                <div className="p-4 flex-1">
-                  <div className="space-y-3 mb-4">
-                    <div className="flex items-center gap-2 text-sm">
-                      <MapPin className="w-4 h-4 text-muted-foreground" />
-                      <span className="text-muted-foreground">
-                        {doctor.experience} years experience
-                      </span>
+                    <div className="flex-1">
+                      <h3 className="font-bold text-foreground text-lg">
+                        {doctor.name}
+                      </h3>
+                      <p className="text-sm text-muted-foreground">
+                        {doctor.specialty}
+                      </p>
                     </div>
-                    <div className="flex items-center gap-2 text-sm">
-                      <div>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        doctor.isFavorite === true
+                          ? removeFromFavorites(doctor?.id)
+                          : addToFavorites(doctor?.id);
+                      }}
+                      disabled={loadingFavoriteId === doctor?.id}
+                      className="text-muted-foreground hover:text-red-500 transition-colors disabled:opacity-50 cursor-pointer"
+                    >
+                      {loadingFavoriteId === doctor?.id ? (
+                        <LoaderCenter size={20} />
+                      ) : (
+                        <Heart
+                          className="w-5 h-5"
+                          fill={doctor?.isFavorite ? "currentColor" : "none"}
+                        />
+                      )}
+                    </button>
+                  </div>
+                  <div className="p-4 flex-1">
+                    <div className="space-y-3 mb-4">
+                      <div className="flex items-center gap-2 text-sm">
                         <MapPin className="w-4 h-4 text-muted-foreground" />
+                        <span className="text-muted-foreground">
+                          {doctor?.experience} years experience
+                        </span>
                       </div>
+                      <div className="flex items-center gap-2 text-sm">
+                        <div>
+                          <MapPin className="w-4 h-4 text-muted-foreground" />
+                        </div>
 
-                      <span className="text-muted-foreground">
-                        {doctor.location}
+                        <span className="text-muted-foreground">
+                          {doctor?.location}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="flex gap-2 mb-4">
+                      <span className="text-xs px-2 py-1 rounded-full bg-secondary/10 text-secondary">
+                        {doctor?.type}
                       </span>
                     </div>
                   </div>
-
-                  <div className="flex gap-2 mb-4">
-                    <span className="text-xs px-2 py-1 rounded-full bg-secondary/10 text-secondary">
-                      {doctor.type}
-                    </span>
-                  </div>
-                </div>
-                <div className="border-t border-border p-4 flex items-center justify-between mt-auto">
-                  <div className="flex justify-between items-center mb-3">
-                    <div>
-                      <p className="text-xs text-muted-foreground">
-                        Consultation Fee
-                      </p>
-                      <p className="font-bold text-foreground">
-                        {doctor.fee.toLocaleString()}
-                      </p>
+                  <div className="border-t border-border p-4 flex items-center justify-between mt-auto">
+                    <div className="flex justify-between items-center mb-3">
+                      <div>
+                        <p className="text-xs text-muted-foreground">
+                          Consultation Fee
+                        </p>
+                        <p className="font-bold text-foreground">
+                          {doctor?.fee?.toLocaleString()}
+                        </p>
+                      </div>
                     </div>
+                    <Button
+                      variant="gradient"
+                      className=""
+                      onClick={() =>
+                        navigate("/doctor-details", {
+                          state: { doctorId: doctor?.id },
+                        })
+                      }
+                    >
+                      Book Now
+                    </Button>
                   </div>
-                  <Button
-                    variant="gradient"
-                    className=""
-                    onClick={() =>
-                      navigate("/doctor-details", {
-                        state: { doctorId: doctor.id },
-                      })
-                    }
-                  >
-                    Book Now
-                  </Button>
-                </div>
-              </Card>
-            ))
+                </Card>
+              );
+            })
           )}
         </div>
-        <div className="text-center mt-12 " onClick={handleLoadMore}>
-          <button className="text-secondary hover:text-primary font-semibold cursor-pointer">
-            {isFetching ? "Loading..." : "Load More Doctors"}
-          </button>
-        </div>
+        {doctors?.data?.totalDoctors > 12 && (
+          <div className="text-center mt-12 " onClick={handleLoadMore}>
+            <button className="text-secondary hover:text-primary font-semibold cursor-pointer">
+              {isFetching ? "Loading..." : "Load More Doctors"}
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
